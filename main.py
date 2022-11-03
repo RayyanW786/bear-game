eb82ec0fe226458627ab6de6b2cc5901d0b87590
RayyanW786 <97803050+RayyanW786@users.noreply.github.com>
2022-11-03 13:27:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-from utils import Bear, UserUtils
+from utils import Bear, UserUtils, UserData
 import json
 from random import shuffle, sample, choice
+
+
 class Game(object):
     def __init__(self, bear, utils):
-        self.bear: Bear = bear()
-        self.userutils: UserUtils = utils()
-        self.choices = ['the first room had strange noises coming from it, however the second room was pitch black, which room do you go into?', 'room 1 was very very cold, however room 2 had a bad feeling about it, which room do you go into?', 'room 1 seems to be full of food, room 2 contains the worlds best drinks, which room do you go into?', 'room 1 and 2 both have a mirror, in both the mirrors you can see the bear, which room do you go to?']
-        self.restart()
-        self.first_menu()
+        self.bear: Bear = bear() # creating a instance of bear class within the Game
+        self.userutils: UserUtils = utils() # instance of UserUtils Class allows us to login etc
+        self.choices = [
+            'the first room had strange noises coming from it, however the second room was pitch black, which room do you go into?',
+            'room 1 was very very cold, however room 2 had a bad feeling about it, which room do you go into?',
+            'room 1 seems to be full of food, room 2 contains the worlds best drinks, which room do you go into?',
+            'room 1 and 2 both have a mirror, in both the mirrors you can see the bear, which room do you go to?']
+        # choices which the Game will randomize and loop over, its easy to add a level by just adding to this list
+        self.restart() #adds thhe variables also we can call reset later on to override the current Game attributes
+        self.first_menu() #starts the main login loop
 
     def restart(self):
         self.lives = 3
         self.gold = 0
         self.highscore = 0
-        shuffle(self.choices)
+        shuffle(self.choices) #randomizes self.choice
 
-    def first_menu(self):
+    def first_menu(self): #displays menu and allows users to log in, register or start the game, log out or view stats
         while True:
             try:
                 try:
                     getattr(self, "userdata")
-                    user_input = input("\n____________________\n1. Start Game\n2. Log Out\n3. Your Stats\n___________________\nEnter your choice 1-3: ")
+                    user_input = input(
+                        "\n____________________\n1. Start Game\n2. Log Out\n3. Your Stats\n___________________\nEnter your choice 1-3: ")
                     user_input = int(user_input)
                     if user_input == 1:
                         self.main()
@@ -29,10 +37,11 @@ class Game(object):
                         print("You have been Logged out...")
                     elif user_input == 3:
                         self.get_stats()
-                        
 
-                except AttributeError:
-                    user_input = input("\n____________________\n1. Register\n2. Log in\n___________________\nEnter your choice 1-2: ")                 
+
+                except AttributeError: #getattr raises AttributeError when the Attribute doesnt exist
+                    user_input = input(
+                        "\n____________________\n1. Register\n2. Log in\n___________________\nEnter your choice 1-2: ")
                     user_input = int(user_input)
                     if user_input == 1:
                         self.user_creation()
@@ -40,30 +49,30 @@ class Game(object):
                         self.login()
                     else:
                         print("Please enter a valid option\nTo quit type quit, exit or stop!")
-            except ValueError:
+            except ValueError: #value error means the program doesnt stop if they input a non integer type
                 if str(user_input).lower() in ["quit", "exit", "stop"]:
                     exit()
-    
+
     def user_creation(self):
-        res = self.userutils.create()
-        if res == None:
-            self.first_menu()
+        res = self.userutils.create() #calls the create function for more info look at the comment made in utils.py line 74
+        if res == None: #None means they typed exit
+            self.first_menu() #puts the user back to the orginal menu loop
         else:
-            self.userdata = res
-    
+            self.userdata = res #sets userdata attr which changes the first menu options to now display start game etc
+
     def login(self):
-        res = self.userutils.login()
-        if res == None:
-            self.first_menu()
+        res = self.userutils.login() #calls the login function for more info look at the comment made in utils.py line 135
+        if res == None: #user typed exit
+            self.first_menu() #puts them back on the orginal loop
         else:
-            self.userdata = res
+            self.userdata = res #sets userdata attr which changes the first menu options to now display start game etc
 
     def gen_gold(self) -> True | False:
         if self.highscore >= 50:
-            return choice([True, False])
-        return False
-    
-    def get_stats(self):
+            return choice([True, False]) #generates the chance of gold appearing
+        return False #we dont need to use else we can simple just return False if the if statement conditions are not met
+
+    def get_stats(self): #displays the stats of the user if they have any
         with open('profiles.json', 'r+') as f:
             data = json.load(f)
         data = data[self.userdata.username]
@@ -75,32 +84,32 @@ class Game(object):
 
     def main(self):
         run = True
-        self.restart()
+        self.restart() #reset the game variables each time they play
         while run == True:
-            for choice in sample(self.choices, len(self.choices)):
+            for choice in sample(self.choices, len(self.choices)): #looping though the choices (randomized)
                 res = input(f"{choice}\nReply with 1, 2, exit or restart!")
                 if res.lower() == "exit":
-                    with open('profiles.json', 'r+') as f:
+                    with open('profiles.json', 'r+') as f: #saving data if they have any
                         fdata = json.load(f)
-                        sdata= fdata[self.userdata.username]
+                        sdata = fdata[self.userdata.username]
                         if self.highscore > sdata['highscore']:
                             fdata[self.userdata.username]['highscore'] = self.highscore
                         fdata[self.userdata.username]['goldscore'] += self.gold
                     with open('profiles.json', 'r+') as f:
                         json.dump(fdata, f)
                     self.first_menu()
-                if res.lower() == 'restart':
-                    self.restart()
-                    run = False
-                    self.main()
-                    break
+                if res.lower() == 'restart': #resets game variables
+                    #self.restart() #we dont need this
+                    #run = False #same with this calling self.main() is enough
+                    self.main() #re calling function
+                    #break #the code shouldnt reach this and so this is useless
                 if res not in ['1', '2']:
-                    print('invalid option, You died')
+                    print('invalid option, You died') #user dies if they enter an invalid option regardless of lives
                     self.first_menu()
                 else:
-                    res = self.bear.generate()
-                    if res == True:
-                        feeling = self.bear.feeling()
+                    res = self.bear.generate() #generates if the bear is in the room or not
+                    if res == True: #True means the bear is infact in the room
+                        feeling = self.bear.feeling() #feeling tells us if the bear wants to kill them or not and why
                         if feeling[1] == False:
                             print(feeling[0])
                             self.lives -= 1
@@ -112,9 +121,9 @@ class Game(object):
                                         fdata[self.userdata.username]['highscore'] = self.highscore
                                     fdata[self.userdata.username]['goldscore'] += self.gold
                                 with open("profiles.json", "r+") as f:
-                                    json.dump(fdata, f)    
+                                    json.dump(fdata, f)
                                 run = False
-                            
+
                         else:
                             self.highscore += 10
                             print(feeling[0])
@@ -122,12 +131,14 @@ class Game(object):
                     else:
                         self.highscore += 10
                         print("Your survived this round!")
-                
-                print(f"Your current score: {self.highscore:,}\nYou have {self.lives} {'lives' if self.lives > 1 else 'life'} left!\n")
+
+                print(
+                    f"Your current score: {self.highscore:,}\nYou have {self.lives} {'lives' if self.lives > 1 else 'life'} left!\n")
             if run != False:
-                res = self.gen_gold()
+                res = self.gen_gold() #generate chance of gold after every loop of self.choices
                 if res == True:
-                    user_input = input("You have found the Gold, do you want to pick it up? y/n\nPicking it up means you win the Game!")
+                    user_input = input(
+                        "You have found the Gold, do you want to pick it up? y/n\nPicking it up means you win the Game!")
                     if user_input.lower() == 'y':
                         self.gold += 1
                         with open('profiles.json', 'r+') as f:
@@ -138,9 +149,8 @@ class Game(object):
                         with open("profiles.json", "r+") as f:
                             json.dump(fdata, f)
                         print("GAME ENDED! CONGRATS")
-                        run = False 
-                    
-                
+                        run = False
+
 
 if __name__ == "__main__":
-    Game(Bear, UserUtils)
+    Game(Bear, UserUtils) #calling Game and passing Bear and UserUtils class.
